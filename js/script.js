21cd9e4d2d52fea11e02c3810ee88080aae5f216
NaujZoriuq <juanq2144@gmail.com>
2024-03-14 17:01:11 -0500

    La Variable v1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por tu mensaje! Nos pondremos en contacto contigo pronto.');
+    document.getElementById('contactForm').reset();
+});
